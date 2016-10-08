5c96ae592cb9bb85c58990b3661141cb910951ef
gyx <gyx@123.com>
2016-10-08 11:34:29 +0800

    我的博客

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+* @Author: Administrator
+* @Date:   2016-10-05 10:25:09
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-10-06 19:11:30
+*/
+
+'use strict';
+
+$(function(){
+	$('.carousel').carousel({
+		interval: 2000
+	});
+});
+
